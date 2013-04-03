5c82050ecf450f3e5f7bb81a4cec0afd0ef3ae8e
erberg <berg.ericr@gmail.com>
2013-04-03 18:49:12 -0400

    Moved Inline Styles

--- index.php
+++ index.php
@@ -5,9 +5,6 @@
 <!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
 
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,800,400italic' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
-
 <head>
 
 	<!-- Basic Page Needs
@@ -31,6 +28,10 @@
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 
+	<!-- Web Fonts
+  ================================================== -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,800,400italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
 	<!-- Favicons
 	================================================== -->
 	<link rel="shortcut icon" href="images/favicon.ico">
@@ -95,7 +96,7 @@
 	</div>
 
 
-	<div class="container" style="margin-bottom:60px;">
+	<div class="container article-container">
 		<div class="three columns">
 			<h1 class="article-title">Artist Portfolio</h1>
 			<h4 class="article-sub">big-picture website</h4>
@@ -104,13 +105,13 @@
 			<p>It's good to be bold. Yes, it takes some confidence to put your images up in large format for all of the internet to see... but I definitely think the payoff is worth it. This is a website I created for just that purpose: to display images as large as possible.</p>
 		    <p>On the more technical side of things, I used this project as an opportunity get a better understanding of the model-view-controller (MVC) pattern. There's no better way to understand something than by building it from scratch, and after having completed this project I'm feeling much more knowledgable about the inner workings of MVC's.</p>
 		</div>
-		<div class="seven columns"><div style="margin-bottom:20px;"><img src="/images/folio.jpg" width="100%"/></div></div>
+		<div class="seven columns"><div class="article-image"><img src="/images/folio.jpg" width="100%"/></div></div>
 
 		<div class="two columns"><ul class="square"><li>PHP</li><li>Javascript</li><li>jQuery</li><li>MVC</li><li>MySQL</li></ul></div>
 	</div><!-- container -->
 
 	
-	<div class="container" style="margin-bottom:80px;">
+	<div class="container article-container">
 		<div class="three columns">
 			<h1 class="article-title">Blackjack</h1>
 			<h4 class="article-sub">game using HTML5</h4>
@@ -119,11 +120,11 @@
 			<p>I thought blackjack would be fun to make because it’s a classic that just about everyone knows how to play. It turned out to be very rewarding project, as well as a great way to learn some new and interesting technologies.</p>
 		    <p>The game has positions for up to six players, and there is a card counting button as a bonus feature. You can turn the odds in your favor by modifying your play based upon the count. The live site and source code are also available, if you’re into that sort of thing.</p>
 		</div>
-		<div class="six columns"><div style="margin-bottom:20px;"><img src="/images/blackjack.jpg" width="100%"/></div></div>
+		<div class="six columns"><div class="article-image"><img src="/images/blackjack.jpg" width="100%"/></div></div>
 		<div class="two columns"><ul class="square"><li>jQuery</li><li>Node.js</li><li>Socket.io</li><li>Twitter Bootstrap</li></ul></div>
 		</div><!-- container -->
 
-		<div class="container" style="margin-bottom:60px;">
+		<div class="container article-container">
 		<div class="three columns">
 			<h1 class="article-title">Conway's Game of Life</h1>
 			<h4 class="article-sub">classic brought to the browser</h4>
@@ -132,13 +133,13 @@
 			<p>I've always been a fascinated by how this simple set of procedures can create so much complexity. The rules create a delicate balance between life and death, and the result is an eco-system simulation in which complex patterns interact, grow, and die. Besides, who's going to pass up such a great opportunity to play god? </p>
 			<p>Alas, my simulated world is a cruel one. After about 6000 generations it's a pretty desolate universe; populated by just a few spasming pixels. Sorry little guys.</p>
 		</div>
-		<div class="seven columns"><div style="margin-bottom:20px;"><img src="/images/gameoflife.jpg" width="100%"/></div></div>
+		<div class="seven columns"><div class="article-image"><img src="/images/gameoflife.jpg" width="100%"/></div></div>
 		<div class="two columns"><ul class="square"><li>PHP</li><li>Javascript</li><li>jQuery</li><li>Twitter Bootstrap</li></ul></div>
 	</div><!-- container -->
 
 
 
-	<div class="container" style="margin-bottom:60px;">
+	<div class="container article-container">
 		<div class="three columns">
 			<h1 class="article-title">Art E-commerce Site</h1>
 			<h4 class="article-sub">art website</h4>
@@ -147,7 +148,7 @@
 			<p>Sometimes I paint in my spare time. For a time, I was contemplating the idea of starting a business producing and selling abstract paintings. Though I eventually realized that production-style art might actually be the least appealing avenue for my creativity, the experience drove me to create a full Joomla! website.</p>
 			<p>My time on this project was split evenly between front-end template design, modifying the Joomla! content management system to suit my needs, and writing articles about art.</p>
 		</div>
-		<div class="six columns"><div style="margin-bottom:20px;"><img src="/images/aura.jpg" width="100%"/></div></div>
+		<div class="six columns"><div class="article-image"><img src="/images/aura.jpg" width="100%"/></div></div>
 		<div class="two columns"><ul class="square"><li>PHP</li><li>Joomla! CMS</li><li>MVC</li></ul></div>
 	</div><!-- container -->
 	
